901d7a7d89d23c27a940bc799ff250b9d5c56cd6
lubyzhang <lubyzhang@lubydeair.home>
2016-09-26 15:12:29 -0500

    second

--- Quiz 2/src/pkgMain/Tuition.java
+++ Quiz 2/src/pkgMain/Tuition.java
@@ -11,34 +11,18 @@ public class Tuition {
 		// Prompt the input of initial tuition and percentage increases
 		System.out.println("Enter the initial tuition cost: ");
 		final double InitialTuition = input.nextDouble();
-		
-		double[] rates = new double[3];
-		for (int i = 2; i < 5; i++){
 			
-			System.out.println("Enter the percentage increase for each year: ");
-			double IncreasingRate = input.nextDouble();
-			rates[i-2] = IncreasingRate;
+		System.out.println("Enter the percentage increase for each year: ");
+		final double IncreasedPercentage = input.nextDouble();
+		
+		double Tuition = 12342;
+		
+		for (int i = 0; i < 3; i++) {
+			double SecondTuition = InitialTuition * (1 + (IncreasedPercentage / 100));
+			Tuition += SecondTuition * (1 + (IncreasedPercentage / 100));
+		
 		}
-		
-		input.close();
-		
-		String ResTotTui = String.format("%.2f", TotalTuition(InitialTuition,rates));
-		System.out.println("The total cost of tuition is $ " + ResTotTui);
-		
-	}
-	
-	public static double TotalTuition(double InitialVal, double[]RateList) {
-		
-		// Calculate the total tuition cost
-		double SemesterCost = InitialVal;
-		double TotalCost = InitialVal;
-		
-		for (int i = 0; i < 3; i++){
-			SemesterCost = SemesterCost * (1 + (RateList[i] / 100));
-			TotalCost += SemesterCost;
-			
-		}
-		return TotalCost;
-	}
-
+		String TotalCost = String.format("%.2f", Tuition);
+		System.out.println("The total cost of tuition is: " + TotalCost);
+	}	
 }
