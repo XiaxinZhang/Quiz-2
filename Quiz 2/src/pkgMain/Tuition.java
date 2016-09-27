94bf681242b33b86f1b8f2f99a60b8368a48a779
lubyzhang <lubyzhang@lubydeair.home>
2016-09-26 20:46:03 -0500

    finally!!!

--- Quiz 2/src/pkgMain/Tuition.java
+++ Quiz 2/src/pkgMain/Tuition.java
@@ -10,19 +10,19 @@ public class Tuition {
 		
 		// Prompt the input of initial tuition and percentage increases
 		System.out.println("Enter the initial tuition cost: ");
-		final double InitialTuition = input.nextDouble();
+		double InitialTuition = input.nextDouble();
 			
 		System.out.println("Enter the percentage increase for each year: ");
-		final double IncreasedPercentage = input.nextDouble();
+		double IncreasedPercentage = input.nextDouble();
 		
-		double Tuition = 12342;
+		double Tuition = InitialTuition;
 		
 		for (int i = 0; i < 3; i++) {
-			double SecondTuition = InitialTuition * (1 + (IncreasedPercentage / 100));
-			Tuition += SecondTuition * (1 + (IncreasedPercentage / 100));
+			Tuition += 	InitialTuition * (1 + (IncreasedPercentage)/100);
+			InitialTuition = InitialTuition * (1 + (IncreasedPercentage)/100);
 		
 		}
 		String TotalCost = String.format("%.2f", Tuition);
-		System.out.println("The total cost of tuition is: " + TotalCost);
+		System.out.println("The total cost of tuition is " + TotalCost);
 	}	
 }
